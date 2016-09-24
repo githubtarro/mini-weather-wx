f61ca306890c41d5d8cdbb48b2ed7bd2444dd950
zhangsan <1966400256@qq.com>
2016-09-24 21:30:51 +0800

    1,2,3

---
+++ app/src/main/java/com/example/administrator_x/myapplication_real/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.administrator_x.myapplication_real;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator_x on 2016/9/20.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
